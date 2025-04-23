a96fe1314b8a6a75979e4c5ca69994baac7b98c8
Gyen Abubakar <indehyde@gmail.com>
2025-04-23 09:55:01 +0000

    refactor: removed redundant FormValidationError component

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,4 +1,3 @@
-export { default as FormValidationError } from './form-validation-error.svelte';
 export { default as PasswordInput } from './password-input.svelte';
 export { default as FormFieldErrors } from './form-field-errors.svelte';
 export { default as FormMessage } from './form-message.svelte';
